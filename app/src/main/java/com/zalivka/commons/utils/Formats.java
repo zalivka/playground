46be9f2aa4d9083e3e7c677bbb86563fb53627ff
zalivka <zalivkamobile@gmail.com>
2016-08-04 08:31:48 +0300

    s

--- app/src/main/java/com/zalivka/commons/utils/Formats.java
+++
@@ -1,9 +0,0 @@
-package com.zalivka.commons.utils;
-
-public class Formats {
-
-    public static final String EXT_ITEM = ".ati";
-    public static final String EXT_BG = ".zip"; // internal format
-    
-
-}
--- app/src/main/java/com/zalivka/commons/utils/StaticContextHolder.java
+++
@@ -1,7 +0,0 @@
-package com.zalivka.commons.utils;
-
-import android.content.Context;
-
-public class StaticContextHolder {
-    public static Context mCtx;
-}
